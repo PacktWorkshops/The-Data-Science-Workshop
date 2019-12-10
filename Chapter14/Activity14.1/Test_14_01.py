51a63a949799b221b9eed9f7c979476ef012fb83
Thomas V Joseph <tvjoseph@users.noreply.github.com>
2019-12-10 12:11:17 +0530

    Unit test updated

---
+++ Chapter14/Activity14.1/Test_14_01.py
@@ -0,0 +1,23 @@
+import unittest
+import import_ipynb
+import pandas as pd
+import pandas.testing as pd_testing
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        import Chapter_14_Activity_14_01_v1
+        self.exercises = Chapter_14_Activity_14_01_v1		
+        self.filename = 'https://raw.githubusercontent.com/PacktWorkshops/The-Data-Science-Workshop/master/Chapter14/Dataset/ad.data'	
+        self.adData = pd.read_csv(self.filename,sep=",",header = None,error_bad_lines=False)        
+        self.dataShape = self.adData.shape 
+
+    def test_file_url(self):
+        self.assertEqual(self.exercises.filename, self.filename)       
+        
+
+    def test_shape(self):
+        self.assertEqual(self.exercises.adData.shape, self.dataShape)		
+
+
+if __name__ == '__main__':
+    unittest.main()
