f72049e0511dc9ce21841c74ccd31d13965704e9
Thomas V Joseph <tvjoseph@users.noreply.github.com>
2020-01-03 20:04:29 +0530

    Unit test file uploaded

---
+++ Chapter15/Exercise15.05/Test_15_05.py
@@ -0,0 +1,23 @@
+import unittest
+import import_ipynb
+import pandas as pd
+import pandas.testing as pd_testing
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        import Exercise_15_05_Ensemble_learning_Bagging_v1_0
+        self.exercises = Exercise_15_05_Ensemble_learning_Bagging_v1_0
+        self.filename = 'https://raw.githubusercontent.com/PacktWorkshops/The-Data-Science-Workshop/master/Chapter15/Dataset/crx.data'
+        self.credData = pd.read_csv(self.filename,sep=",",header = None,na_values = "?")
+        self.dataShape = self.credData.shape
+
+    def test_file_url(self):
+        self.assertEqual(self.exercises.filename, self.filename)
+        
+
+    def test_shape(self):
+        self.assertEqual(self.exercises.credData.shape, self.dataShape)
+
+
+if __name__ == '__main__':
+    unittest.main()
